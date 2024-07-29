0865e814bd5c37094cc7809c1451a0ac22e16308
andrew-surrena <andrewsurrena@gmail.com>
2024-07-29 17:08:13 -0500

    added functionality for creating readme.md and created functions for applying license badge and links

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
 import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
 // TODO: Create an array of questions for user input
 const questions = [
-    inquirer
-  .prompt([
+    
     //Title
     {
         type: 'input',
@@ -17,7 +17,6 @@ const questions = [
         message: 'Please describe your project in a few sentences.',
         name: 'description',
       },
-    //Table of Contents
     
     //Installation Instructions
     {
@@ -29,40 +28,68 @@ const questions = [
     {
         type: 'input',
         message: 'Please provide instructions for using your application',
-        name: 'installationGuide',
+        name: 'usage',
       },
     //License Choice
     {
         type: 'list',
         message: `Choose a license to add to your Readme. If you would prefer not to add a licence, please select "skip". `,
-        name: 'installationGuide',
+        name: 'license',
+        choices: ['Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'Mozilla Public License', 'The Unlicense', 'skip']
       },
     //Contributing
+    {
+      type: 'input',
+      message: 'Please add a list of contributers.',
+      name: 'contrubutors',
+    },
     //Tests
+    {
+      type: 'input',
+      message: `Please describe any tests for your application. To skip, press ENTER.`,
+      name: 'tests',
+    },
     //Questions including email & git link
+    {
+      type: 'input',
+      message: `Please add a valid email address.`,
+      name: 'email',
+  }
+  ,
+    {
+      type: 'input',
+      message: `Please add a link to your github.`,
+      name: 'github',
+    },
     
-    
-    
-    
-  ])
-  .then(function(response) {
-    if(response.confirm === response.password)
-      {console.log(colors.green('Success!')); 
-    let userdata = `username: ${response.username}
-      password: ${response.password}`
-    fs.appendFile('log.txt', `${userdata}\n`, (err) =>
-        err ? console.error(err) : console.log('Commit logged!'))}
-      else {console.log(colors.red('You forgot your password already?!'))}})
+  
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
+
+function writeToFile(fileName, userData) {
+    fs.writeFile(fileName, userData, (err) =>
 err ? console.error(err) : console.log('Readme created!') );
 }
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+  inquirer
+  .prompt(questions)
+  .then(function(response) {
+    const fileName = `${response.title.toLowerCase().split(' ').join('')}_README.md`;
+    function generateReadme(response) {
+      if (response.license === 'skip') {
+        response.license = 'No License'
+      } 
+      if (response.tests === '') {
+        response.tests = 'No tests have been run at this time'
+      }
+    }
+    const readmeContent = generateMarkdown(response);
+  writeToFile(`./generated_files/README.md`, readmeContent); 
+  console.log('Thank you for submitting responses, generating README.md') ;
+    })
+}
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,74 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  let licenseBadge;
+  if (license === 'Apache License 2.0'){licenseBadge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`}
+  else if (license === 'MIT License'){licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`}
+  else if (license === 'Boost Software License 1.0'){licenseBadge = `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`}
+  else if (license === 'Mozilla Public License'){licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`}
+  else if (license === 'The Unlicense'){licenseBadge = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`}
+  else {licenseBadge = ``};return licenseBadge;
+}
+//'Apache License 2.0', 'MIT License', 'Boost Software License 2.0', 'Mozilla Public License', 'The Unlicense', 'skip'
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink;
+  if (license === 'Apache License 2.0'){licenseLink = `[Apache-2.0](https://opensource.org/licenses/Apache-2.0)`}
+  else if (license === 'MIT License'){licenseLink = `[MIT](https://opensource.org/licenses/MIT)`}
+  else if (license === 'Boost Software License 1.0'){licenseLink = `[BSL-1.0](https://www.boost.org/LICENSE_1_0.txt)`}
+  else if (license === 'Mozilla Public License'){licenseLink = `[MPL-2.0](https://opensource.org/licenses/MPL-2.0)`}
+  else if (license === 'The Unlicense'){licenseLink = `[Unlicense](http://unlicense.org/)`}
+  else {licenseLink = ``}; return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+  let licenseLink = renderLicenseLink(data.license);
+  return `## License
+  ${data.license}
+  ${licenseLink}
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  let licenseBadge = renderLicenseBadge(data.license);
+renderLicenseBadge(data.license);
+renderLicenseLink(data.license);
+console.log(data);
+  return `# ${data.title} ${licenseBadge}
+    
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributors](#contributors)
+  - [Tests](#tests)
+  - [Questions](#questions)
+      
+  ## Installation
+  ${data.installationGuide}
+      
+  ## Usage
+  ${data.usage}
+      
+  ${renderLicenseSection(data)}
+      
+  ## Contributors
+  ${data.contributors}
+      
+  ## Tests
+  ${data.tests}
+      
+  ## Questions
+  If you have any questions, please email me at ${data.email}. You can find more of my work at ${data.github}.
+  `;
 }
 
 export default generateMarkdown;
+
